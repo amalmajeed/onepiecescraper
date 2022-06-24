9a293474c6c2c353ef1d89d531b555cf47ce3ca7
Amal Majeed <amalmajeed7@gmail.com>
2022-06-23 21:45:13 -0600

    new dependency added

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,3 +1,14 @@
+# This module scrapers the table mapping from the URL into a pandas dataframe 
 
+# Input : link to the URL
+# Process : scrape the HTML and move it to a pandas dataframe and extract only the necessary columns
+# Output : either a dictionary from number (anime episode) to list(manga chapters) or 
+#           to object(name of ep, list of manga chaps) mapping 
+
+
+import pandas as pd
 
 map_URL = "https://listfist.com/list-of-one-piece-episode-to-chapter-conversion"
+
+onepieceTable = pd.read_html(map_URL)
+print(f"The total number of tables is : {len(onepieceTable)}")
